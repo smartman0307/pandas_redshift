90e984915a4bf3ab1704674c968f6b477925dce2
smartman0307 <skybolt0307@gmail.com>
2018-02-12 19:08:03 -0500

    adding the ability to pass session token to boto3.

--- pandas_redshift/core.py
+++ pandas_redshift/core.py
@@ -6,10 +6,9 @@ import psycopg2
 import boto3
 import sys
 import os
-import pandas._libs.lib as lib
-from pandas.core.dtypes.missing import isna
 
-def connect_to_redshift(dbname, host, user, password, port = 5439):
+
+def connect_to_redshift(dbname, host, user, port = 5439, **kwargs):
     # connect to redshift
     global connect
     global cursor
@@ -17,16 +16,17 @@ def connect_to_redshift(dbname, host, user, password, port = 5439):
                                         host = host,
                                         port = port,
                                         user = user,
-                                        password = password)
+                                        **kwargs)
 
     cursor = connect.cursor()
 
 
-def connect_to_s3(aws_access_key_id, aws_secret_access_key, bucket, subdirectory = None):
-    global s3, s3_bucket_var, s3_subdirectory_var, aws_1, aws_2
+def connect_to_s3(aws_access_key_id, aws_secret_access_key, bucket, subdirectory = None, **kwargs):
+    global s3, s3_bucket_var, s3_subdirectory_var, aws_1, aws_2, aws_token
     s3 = boto3.resource('s3',
                         aws_access_key_id = aws_access_key_id,
-                        aws_secret_access_key = aws_secret_access_key)
+                        aws_secret_access_key = aws_secret_access_key,
+                        **kwargs)
     s3_bucket_var = bucket
     if subdirectory is None:
         s3_subdirectory_var = ''
@@ -34,59 +34,15 @@ def connect_to_s3(aws_access_key_id, aws_secret_access_key, bucket, subdirectory
         s3_subdirectory_var = subdirectory + '/'
     aws_1 = aws_access_key_id
     aws_2 = aws_secret_access_key
+    if kwargs.get('aws_session_token'):
+        aws_token = kwargs.get('aws_session_token')
 
-    
+
 def redshift_to_pandas(sql_query):
     # pass a sql query and return a pandas dataframe
     cursor.execute(sql_query)
     columns_list = [desc[0] for desc in cursor.description]
     data = pd.DataFrame(cursor.fetchall(), columns = columns_list)
-    # try to coerce dtypes
-    
-    def __coerce_col_dtype__(input_col):
-        """
-        Infer datatype of a pandas column, process only if the column dtype is object.
-        input:   col: a pandas Series representing a df column.
-        
-        this function is adapted from: pandas.io.sql.SQLTable._get_notna_col_dtype
-            
-            Infer datatype of the Series col.  In case the dtype of col is 'object'
-            and it contains NA values, this infers the datatype of the not-NA
-            values.  Needed for inserting typed data containing NULLs, GH8778.
-        
-        """
-        # sample the first 10 000  rows to determine type. 
-        col = input_col.dropna().unique()[:10000]
-        
-        if col.dtype =="object":
-            # try numeric
-            try:
-                col_new = pd.to_datetime(col)
-                return col_new.dtype
-            except:
-                try:
-                    col_new = pd.to_numeric(col)
-                    return col_new.dtype
-                except:
-                    try:
-                        col_new = pd.to_timedelta(col)
-                        return col_new.dtype
-                    except:
-                        return "object"
-        else:
-            output = lib.infer_dtype(col)
-            # translate to numpy dtypes
-            to_numpy_dtypes = {
-                    "integer":"int64",
-                    "floating":"float64",
-                    "datetime64":"datetime64[ns]", 
-                    "string":"object"
-             }
-            return to_numpy_dtypes.get(output, output)
-  
-    dtypes = {x:_get_notna_col_dtype(data[x]) for x in data.columns }
-    data.astype(dtype=dtypes)
-
     return data
 
 
@@ -112,14 +68,11 @@ def pandas_to_redshift(data_frame,
         if save_local == True:
             data_frame.to_csv(csv_name, index = index, sep = delimiter)
             print('saved file {0} in {1}'.format(csv_name, os.getcwd()))
-            data_send = open(csv_name, 'rb')
-            s3.Bucket(s3_bucket_var).put_object(Key= s3_subdirectory_var + csv_name, Body = data_send)
-            print('saved file {0} in bucket {1}'.format(csv_name, s3_subdirectory_var + csv_name))
-        else:
-            csv_buffer = StringIO()
-            data_frame.to_csv(csv_buffer, index = index, sep = delimiter)
-            s3.Bucket(s3_bucket_var).put_object(Key= s3_subdirectory_var + csv_name, Body = csv_buffer.getvalue())
-            print('saved file {0} in bucket {1}'.format(csv_name, s3_subdirectory_var + csv_name))
+        # SEND DATA TO S3    
+        csv_buffer = StringIO()
+        data_frame.to_csv(csv_buffer, index = index, sep = delimiter)
+        s3.Bucket(s3_bucket_var).put_object(Key= s3_subdirectory_var + csv_name, Body = csv_buffer.getvalue())
+        print('saved file {0} in bucket {1}'.format(csv_name, s3_subdirectory_var + csv_name))
         # CREATE AN EMPTY TABLE IN REDSHIFT
         if index == True:
             columns = list(data_frame.columns)
@@ -141,17 +94,31 @@ def pandas_to_redshift(data_frame,
             connect.commit()
         # CREATE THE COPY STATEMENT TO SEND FROM S3 TO THE TABLE IN REDSHIFT
         bucket_name = 's3://{0}/{1}'.format(s3_bucket_var, s3_subdirectory_var + csv_name)
-        s3_to_sql = """
-        copy {0}
-        from '{1}'
-        delimiter '{2}'
-        ignoreheader 1
-        csv quote as '{3}'
-        dateformat '{4}'
-        timeformat '{5}'
-        access_key_id '{6}'
-        secret_access_key '{7}';
-        """.format(redshift_table_name, bucket_name, delimiter, quotechar, dateformat, timeformat, aws_1, aws_2)
+        if aws_token:
+            s3_to_sql = """
+            copy {0}
+            from '{1}'
+            delimiter '{2}'
+            ignoreheader 1
+            csv quote as '{3}'
+            dateformat '{4}'
+            timeformat '{5}'
+            access_key_id '{6}'
+            secret_access_key '{7}'
+            session_token '{8}';
+            """.format(redshift_table_name, bucket_name, delimiter, quotechar, dateformat, timeformat, aws_1, aws_2, aws_token)
+        else:
+            s3_to_sql = """
+            copy {0}
+            from '{1}'
+            delimiter '{2}'
+            ignoreheader 1
+            csv quote as '{3}'
+            dateformat '{4}'
+            timeformat '{5}'
+            access_key_id '{6}'
+            secret_access_key '{7}';
+            """.format(redshift_table_name, bucket_name, delimiter, quotechar, dateformat, timeformat, aws_1, aws_2)
         print(s3_to_sql)
         # send the file
         print('FILLING THE TABLE IN REDSHIFT')
